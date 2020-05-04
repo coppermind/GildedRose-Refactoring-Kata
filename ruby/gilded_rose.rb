99f5384470ef06bf1a61f34dfaa398b4c2423d2b
Paul Corpuz <paul.corpuz@gmail.com>
2020-05-04 23:35:38 +0800

    Refactor for brevity

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,48 +4,32 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+      next if item.name == 'Sulfuras, Hand of Ragnaros'
+
+      if item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if item.quality < 50
+          item.quality = item.quality + 1
+          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+            item.quality = item.quality + 1 if item.sell_in < 11 && item.quality < 50
+            item.quality = item.quality + 1 if item.sell_in < 6 && item.quality < 50
           end
         end
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+        item.quality = item.quality - 1 if item.quality > 0
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+
+      item.sell_in = item.sell_in - 1
+
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
+        if item.name == 'Aged Brie'
+          item.quality = item.quality + 1 if item.quality < 50
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
+          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+            item.quality = item.quality - item.quality
+          else
+            item.quality = item.quality - 1 if item.quality > 0
           end
         end
       end
@@ -62,7 +46,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
